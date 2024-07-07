dbfb6fddebcafb85244ffe37bbaf1c8b6905a6cc
Henrik Saura Engen <henriksen.engen@gmail.com>
2024-07-07 19:32:51 +0000

    Changed hover text for position icons (permanent position and active position descriptions)

--- src/pages/user/viewer/positions.jsx
+++ src/pages/user/viewer/positions.jsx
@@ -19,8 +19,8 @@ export const Position = ({ position, func, positionName }) => {
 
 	return (
 		<SelectableTableRow onClick={locked ? null : func}>
-			<TableCell flex="0 1.3rem"  mobileHide center><IconContainer hidden={!locked} color="#ef6c00"><FontAwesomeIcon icon={faLock} title="Stillingen er ikke knyttet til et arrangement, og kan ikke slettes." /></IconContainer></TableCell>
-			<TableCell flex="0 1.3rem"  mobileHide center><IconContainer hidden={!active} color="#43a047"><FontAwesomeIcon icon={faCircleCheck} title="Stillingen er ikke knyttet til et arrangement, og kan ikke slettes." /></IconContainer></TableCell>
+			<TableCell flex="0 1.3rem"  mobileHide center><IconContainer hidden={!locked} color="#ef6c00"><FontAwesomeIcon icon={faLock} title="Stillingen er ikke tilknyttet et arrangement og er permanent. Stillingen kan ikke slettes." /></IconContainer></TableCell>
+			<TableCell flex="0 1.3rem"  mobileHide center><IconContainer hidden={!active} color="#43a047"><FontAwesomeIcon icon={faCircleCheck} title="Stillingen er aktiv og gir rettigheter til brukeren." /></IconContainer></TableCell>
 			<TableCell flex="0 1px"     mobileHide fillGray />
 			<TableCell flex="3"                   >{positionName}</TableCell>
 			<TableCell flex="2"                   >{active ? <>Nåværende stilling</> : <>Tidligere stilling</>}</TableCell>
