52a711502628f23f29b51d6ed3b3932e7a3282e9
Henrik Saura Engen <henriksen.engen@gmail.com>
2025-01-26 16:54:34 +0000

    Roles and permissions update - Changed design in permissionList.jsx, and memberList.jsx, added position details.jsx, added and modified components in dashboard.jsx, added new colors in theme.js

--- src/pages/position/view/memberList.jsx
+++ src/pages/position/view/memberList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import { TableCell, IconContainer, SelectableTableRow, Table, TableHead, TableBody, TableRow } from '../../../components/table';
-import { InnerContainer, InnerContainerRow, InnerContainerTitle, InputCheckbox, InputContainer, InputLabel, InputSelect } from '../../../components/dashboard';
+import { DropdownCardContainer, DropdownCardContent, DropdownCardHeader, InnerContainer, InnerContainerRow, InnerContainerTitle, InputCheckbox, InputContainer, InputLabel, InputSelect, PanelButton, RowBorder, SpanLink } from '../../../components/dashboard';
 import { UserSearch } from "../../../components/userSearch";
 import { FormButton } from '../../../components/form';
 
@@ -12,11 +12,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router-dom";
 import { PageLoading } from "../../../components/pageLoading";
 
+const messages = {
+    "position.addUserTitle": "Legg til bruker",
+    "position.addUserDescription": ["Du kan bare gi ut stillingen for nåværende arrangement.", <br/>, "De vil ikke få en e-post for å informere om den nye stillingen.", <br/>, "Brukeren blir nødt til å logge ut og inn for at den nye stillingen skal tre i kraft."],
+    "position.filterByEventTitle": "Vis medlemmer fra annet arrangement"
+}
+
+
 export const PositionMemberList = ({ position, refresh }) => {
     const [ loading, setLoading ] = useState(true);
     const [ visibleUUID, setVisibleUUID ] = useState(false);
     const [ member, setNewMember ] = useState(null);
     const [ isAddingMember, setIsAddingMember ] = useState(false);
+    const [ addUserDropdownState, setAddUserDropdownState ] = useState(false);
+    const [ filterEventDropdownState, setFilterEventDropdownState ] = useState(false);
 
     const [ currentEvent, setCurrentEvent ] = useState();
     const [ events, setEvents ] = useState();
@@ -65,67 +74,108 @@ export const PositionMemberList = ({ position, refresh }) => {
         const position_mappings = position.position_mappings.filter(currentEventFilter)
         return (
             <>
-                <InnerContainer extramargin>
-                    <InnerContainerRow>
-                        <InnerContainer flex="1">
-                            <InnerContainerTitle>Legg til nytt medlem</InnerContainerTitle>
-                            <InnerContainer>Du kan bare gi ut stillingen for nåværende arrangement. Merk at de ikke vil få en e-post for å informere dem den nye stillingen.</InnerContainer>
-                            
+                <InnerContainer>
+                    <DropdownCardContainer desktopHide>
+                        <DropdownCardHeader title={messages["position.addUserTitle"]} dropdownState={addUserDropdownState} onClick={() => setAddUserDropdownState(!addUserDropdownState)} />
+                        <DropdownCardContent dropdownState={addUserDropdownState}>
+                            {messages["position.addUserDescription"]}
                             <UserSearch onUserSelected={setNewMember} onChange={() => setNewMember(null)}/>
                             {
                                 isAddingMember ? (
                                     <PageLoading />
                                 ) : (
-                                    <FormButton disabled={!member} type="submit" onClick={() => addMember()}>Legg til medlem</FormButton>
+                                    <PanelButton fillWidth disabled={!member} type="submit" onClick={() => addMember()}>Legg til</PanelButton>
                                 )
                             }
-                        </InnerContainer>
-                        <InnerContainer flex="1">
-                            <InnerContainerRow nopadding nowrap>
-                                <InputContainer column mobileNoMargin>
-                                    <InnerContainerTitle>Vis medlemmer for et annet arrangement</InnerContainerTitle>
-                                    <InputLabel small>Arrangement</InputLabel>
-                                    <InputSelect value={currentViewingEvent} onChange={updateViewingEvent}>
-                                        {
-                                            events.map((event) => (<option value={event.uuid}>{event.name} {event.uuid == currentEvent.uuid ? "(Nåværende)" : null}</option>))
-                                        }
-                                    </InputSelect>
-                                </InputContainer>
-                            </InnerContainerRow>
-                        </InnerContainer>
-                        <InnerContainer mobileHide flex="1" />
+                        </DropdownCardContent>
+                    </DropdownCardContainer>
+                    <DropdownCardContainer desktopHide>
+                        <DropdownCardHeader title={messages["position.filterByEventTitle"]} dropdownState={filterEventDropdownState} onClick={() => setFilterEventDropdownState(!filterEventDropdownState)} />
+                        <DropdownCardContent dropdownState={filterEventDropdownState}>
+                            <InnerContainer flex="2" nopadding>
+                            <InputLabel small>Arrangement</InputLabel>
+                            <InputSelect value={currentViewingEvent} onChange={updateViewingEvent}>
+                                {
+                                    events.map((event) => (<option value={event.uuid}>{event.name} {event.uuid == currentEvent.uuid ? "(Nåværende)" : null}</option>))
+                                }
+                            </InputSelect>
+                            </InnerContainer>
+                        </DropdownCardContent>
+                    </DropdownCardContainer>
+
+
+
+
+                    <InnerContainerRow mobileHide>
+                        <InnerContainerRow mobileHide>
+                            <InnerContainer flex="4" nopadding>
+                                <InnerContainerTitle>{messages["position.addUserTitle"]}</InnerContainerTitle>
+                                {messages["position.addUserDescription"]}
+                            </InnerContainer>
+                            <RowBorder />
+                            <InnerContainer flex="2" nopadding>
+                                <UserSearch onUserSelected={setNewMember} onChange={() => setNewMember(null)}/>
+                            </InnerContainer>
+                            <InnerContainer flex="1" nopadding>
+                                {
+                                    isAddingMember ? (
+                                        <PageLoading />
+                                    ) : (
+                                        <PanelButton fillWidth disabled={!member} type="submit" onClick={() => addMember()}>Legg til</PanelButton>
+                                    )
+                                }
+                            </InnerContainer>
+                        </InnerContainerRow>
+
+                        <InnerContainerRow>
+                            <InnerContainer flex="4" nopadding>
+                            </InnerContainer>
+                            <RowBorder />
+                            <InnerContainer flex="2" nopadding>
+                                <InputLabel small>Vis medlemmer fra annet arrangement</InputLabel>
+                                <InputSelect value={currentViewingEvent} onChange={updateViewingEvent}>
+                                    {
+                                        events.map((event) => (<option value={event.uuid}>{event.name} {event.uuid == currentEvent.uuid ? "(Nåværende)" : null}</option>))
+                                    }
+                                </InputSelect>
+                            </InnerContainer>
+                            <InnerContainer flex="1" />
+                        </InnerContainerRow>
                     </InnerContainerRow>
                 </InnerContainer>
 
                 <InnerContainer>
-                    <InnerContainer mobileHide>
-                        <InputCheckbox label="Vis UUID" value={visibleUUID} onChange={() => setVisibleUUID(!visibleUUID)} />
-                    </InnerContainer>
-                    <Table>
-                        <TableHead border>
-                            <TableRow>
-                                <TableCell mobileHide flex="5" visible={!visibleUUID}>UUID</TableCell>
-                                <TableCell mobileFlex="3" flex="3">Navn</TableCell>
-                                <TableCell mobileFlex="2" flex="3">Brukernavn</TableCell>
-                                <TableCell mobileHide flex="0 24px" />
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {
-                                position_mappings.map((position_mapping) => {
-                                    const user = position_mapping.user
-                                    return (
-                                        <SelectableTableRow onClick={e => {history.push(`/user/${user.uuid}`)}} title="Trykk for å åpne">
-                                            <TableCell mobileHide consolas flex="5" visible={!visibleUUID}>{ user.uuid }</TableCell>
-                                            <TableCell mobileFlex="3" flex="3">{ user.lastname + ", " + user.firstname }</TableCell>
-                                            <TableCell mobileFlex="2" flex="3">{ user.username }</TableCell>
-                                            <TableCell mobileHide flex="0 24px"><IconContainer><FontAwesomeIcon icon={faArrowRight}/></IconContainer></TableCell>
-                                        </SelectableTableRow>
-                                    )
-                                })
-                            }
-                        </TableBody>
-                    </Table>
+                    <InnerContainerRow>
+                        <InnerContainerRow>
+                            <InnerContainer>
+                                <Table>
+                                    <TableHead border>
+                                        <TableRow>
+                                            <TableCell mobileHide flex="3" visible={!visibleUUID}>UUID <SpanLink onClick={() => setVisibleUUID(!visibleUUID)}>{visibleUUID ? "(Skjul UUID)" : null}</SpanLink></TableCell>
+                                            <TableCell mobileFlex="2" flex="2">Brukernavn <SpanLink mobileHide onClick={() => setVisibleUUID(!visibleUUID)}>{visibleUUID ? null : "(Vis UUID)"}</SpanLink></TableCell>
+                                            <TableCell mobileFlex="3" flex="4">Navn</TableCell>
+                                            <TableCell mobileHide flex="0 24px" />
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {
+                                            position_mappings.map((position_mapping) => {
+                                                const user = position_mapping.user
+                                                return (
+                                                    <SelectableTableRow onClick={e => {history.push(`/user/${user.uuid}`)}} title="Trykk for å åpne">
+                                                        <TableCell mobileHide consolas flex="3" visible={!visibleUUID}>{ user.uuid }</TableCell>
+                                                        <TableCell mobileFlex="2" flex="2">{ user.username }</TableCell>
+                                                        <TableCell mobileFlex="3" flex="4">{ user.firstname + ", " + user.lastname}</TableCell>
+                                                        <TableCell mobileHide flex="0 24px"><IconContainer><FontAwesomeIcon icon={faArrowRight}/></IconContainer></TableCell>
+                                                    </SelectableTableRow>
+                                                )
+                                            })
+                                        }
+                                    </TableBody>
+                                </Table>
+                            </InnerContainer>
+                        </InnerContainerRow>
+                    </InnerContainerRow>
                 </InnerContainer>
             </>
         )
