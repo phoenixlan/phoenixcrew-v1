9e83689df2100d38268418b46508524d7e498a21
Henrik Saura Engen <henriksen.engen@gmail.com>
2025-04-18 14:17:30 +0000

    Disabled APPLICATIONS tab because it doesn't work, needs to be fixed in another PR.

--- src/pages/user/viewer/index.jsx
+++ src/pages/user/viewer/index.jsx
@@ -88,7 +88,7 @@ export const ViewUser = (props) => {
                     <DashboardBarElement active={activeContent === TABS.POSITIONS} onClick={() => setActiveContent(TABS.POSITIONS)}>Stillinger</DashboardBarElement>
                     <DashboardBarElement active={activeContent === TABS.TICKETS} onClick={() => setActiveContent(TABS.TICKETS)}>Billetter</DashboardBarElement>
                     <DashboardBarElement active={activeContent === TABS.INTEGRATIONS} onClick={() => setActiveContent(TABS.INTEGRATIONS)}>Eksterne tilkoblinger</DashboardBarElement>
-                    <DashboardBarElement active={activeContent === TABS.APPLICATIONS} onClick={() => setActiveContent(TABS.APPLICATIONS)}>Søknader</DashboardBarElement>
+                    <DashboardBarElement disabled active={activeContent === TABS.APPLICATIONS} title="Ikke tilgjengelig">Søknader</DashboardBarElement>
                 </DashboardBarSelector>
                 
                 <DashboardContent visible={activeContent === TABS.USER_DETAILS}>
