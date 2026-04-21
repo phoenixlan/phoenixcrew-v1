966f506bc534cb2905095b3406fc24601312d2e7
Henrik Saura Engen <henriksen.engen@gmail.com>
2026-04-21 16:53:45 +0000

    Removed unused code, added error and permission handling event/view/index.jsx

--- src/pages/event/view/index.jsx
+++ src/pages/event/view/index.jsx
@@ -1,14 +1,12 @@
-import React , { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
-import { getEvent, getEventTicketTypes, addEventTicketType, TicketType, Seatmap } from "@phoenixlan/phoenix.js";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExclamationTriangle }  from '@fortawesome/free-solid-svg-icons'
-import { PageLoading } from "../../../components/pageLoading"
-import { DashboardBarElement, DashboardBarSelector, DashboardContent, DashboardHeader, DashboardSubtitle, DashboardTitle, InnerContainer, InnerContainerRow, InnerContainerTitle, InputCheckbox, InputContainer, InputElement, InputLabel, InputSelect, LabelWarning } from "../../../components/dashboard";
-import { FormButton } from "../../../components/form";
+import { getEvent, getEventTicketTypes, TicketType, Seatmap } from "@phoenixlan/phoenix.js";
+import { PageLoading } from "../../../components/pageLoading";
+import { DashboardBarElement, DashboardBarSelector, DashboardContent, DashboardHeader, DashboardSubtitle, DashboardTitle, InnerContainer, InnerContainerRow } from "../../../components/dashboard";
 import { EventDetails } from "./details";
 import { EventTickets } from "./tickets";
 import { AuthenticationContext } from "../../../components/authentication";
+import { Notice } from "../../../components/containers/notice";
 
 export const EventViewer = () => {
 
@@ -16,17 +14,15 @@ export const EventViewer = () => {
     const authContext = useContext(AuthenticationContext);
 
     // Function availibility control:
-    const viewEvent = authContext.roles.includes("admin") || authContext.roles.includes("event_admin")
-
-    const [event, setEvent] = useState([]);
+    const viewEvent = authContext.roles.includes("admin") || authContext.roles.includes("event_admin");
+    const [error, setError] = useState(false);
+    const [event, setEvent] = useState(null);
     const [ticketTypes, setTicketTypes] = useState([]);
     const [eventTicketTypes, setEventTicketTypes] = useState([]);
     const [seatMaps, setSeatmaps] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const [activeContent, setActiveContent] = useState(1);
-//    const [cancelEventCheck, setCancelEventCheck] = useState(false);
-//    const [cancelEventReason, setCancelEventReason] = useState(null);
 
     const [selectedTicketTypeUuid, setSelectedTicketTypeUuid] = useState("");
 
@@ -34,13 +30,14 @@ export const EventViewer = () => {
 
     const load = async () => {
         if(viewEvent) {
-            const [ event, ticketTypes, seatMaps ] = await Promise.all([
+            const [ event, ticketTypes, seatMaps, eventTicketTypes ] = await Promise.all([
                     getEvent(uuid),
                     TicketType.getTicketTypes(),
                     Seatmap.getSeatmaps(),
+                    getEventTicketTypes(uuid)
                 ]
             )
-            const eventTicketTypes = await getEventTicketTypes(event.uuid);
+
             const legalTicketTypes = ticketTypes.filter(ticketType => {
                 return ticketType.price !== 0 && eventTicketTypes.filter((type) => type.uuid === ticketType.uuid).length === 0;
             });
@@ -48,7 +45,7 @@ export const EventViewer = () => {
             if(legalTicketTypes.length > 0) {
                 setSelectedTicketTypeUuid(legalTicketTypes[0].uuid);
             } else {
-                setSelectedTicketTypeUuid("")
+                setSelectedTicketTypeUuid("");
             }
 
             setTicketTypes(legalTicketTypes);
@@ -56,68 +53,77 @@ export const EventViewer = () => {
             setEventTicketTypes(eventTicketTypes); 
             setSeatmaps(seatMaps);
         }
-        setLoading(false);
     }
 
     useEffect(async () => {
-        await load();
+        await load().catch(e => {
+            setError(e);
+            console.error(e);
+        })
+        setLoading(false);
     }, []);
-
-    // Function to handle when the checkbox for cancelling the event is clicked.
-/*    const changeCancelEventCheck = () => {
-        if(cancelEventCheck) {
-            setCancelEventCheck(false);
-            setCancelEventReason(null);
-        } else {
-            setCancelEventCheck(true);
-            setCancelEventReason("");
-        }
-    }
-*/
-/*    const changeSelectedTicketType = (event) => {
-        setSelectedTicketTypeUuid(event.target.value)
-    }
-*/
-/*    const addTicketType = async (e) => {
-        e.preventDefault()
-        if(selectedTicketTypeUuid !== "") {
-            console.log(selectedTicketTypeUuid)
-            await addEventTicketType(event.uuid, selectedTicketTypeUuid)
-            reload();
-        } else {
-            alert("No ticket type left to add")
-        }
-    }
-*/
     
     // View loading page if loading is true
     if(loading) {
         return (<PageLoading />)
+    } else if(viewEvent) {
+        if(event)
+            return (
+                <>
+                    <DashboardHeader>
+                        <DashboardTitle>
+                            Arrangement
+                        </DashboardTitle>
+                        <DashboardSubtitle>
+                            {event.name}
+                        </DashboardSubtitle>
+                    </DashboardHeader>
+
+                    <DashboardBarSelector border>
+                        <DashboardBarElement active={activeContent == 1} onClick={() => setActiveContent(1)}>Generelt</DashboardBarElement>
+                        <DashboardBarElement active={activeContent == 2} onClick={() => setActiveContent(2)}>Billettstyring</DashboardBarElement>
+                    </DashboardBarSelector>
+
+                    <DashboardContent visible={activeContent == 1}>
+                        <EventDetails event={event} refresh={load} />
+                    </DashboardContent>
+
+                    <DashboardContent visible={activeContent == 2}>
+                        <EventTickets event={event} ticketTypes={ticketTypes} eventTicketTypes={eventTicketTypes} seatMaps={seatMaps} refresh={load} />
+                    </DashboardContent>
+                </>
+            )
+        else {
+            return (
+                <>
+                    <DashboardHeader border>
+                        <DashboardTitle>
+                            Arrangement
+                        </DashboardTitle>
+                    </DashboardHeader>
+
+                    <DashboardContent>
+                        <Notice type="error" visible>
+                            Det oppsto en feil ved henting av informasjon for dette arrangementet.<br />
+                            {error.message}
+                        </Notice>
+                    </DashboardContent>
+                </>
+            )
+        }
+    } else {
+        return (
+            <>
+                <DashboardContent>
+                    <InnerContainer rowgap>
+                        <InnerContainerRow>
+                            <Notice type="error" visible>
+                                Du har ikke tilgang til å administrere arrangementer.
+                            </Notice>
+                        </InnerContainerRow>
+                    </InnerContainer>
+                </DashboardContent>
+            </>
+        )
     }
-    
-    return (
-        <>
-            <DashboardHeader>
-                <DashboardTitle>
-                    Arrangement
-                </DashboardTitle>
-                <DashboardSubtitle>
-                    {event.name}
-                </DashboardSubtitle>
-            </DashboardHeader>
-
-            <DashboardBarSelector border>
-                <DashboardBarElement active={activeContent == 1} onClick={() => setActiveContent(1)}>Generelt</DashboardBarElement>
-                <DashboardBarElement active={activeContent == 2} onClick={() => setActiveContent(2)}>Billettstyring</DashboardBarElement>
-            </DashboardBarSelector>
-
-            <DashboardContent visible={activeContent == 1}>
-                <EventDetails event={event} />
-            </DashboardContent>
-
-            <DashboardContent visible={activeContent == 2}>
-                <EventTickets event={event} ticketTypes={ticketTypes} eventTicketTypes={eventTicketTypes} seatMaps={seatMaps} refresh={load} />
-            </DashboardContent>
-        </>
-    )
 }
